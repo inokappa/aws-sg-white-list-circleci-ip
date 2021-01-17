5096aab92b88880a1e41e8016b168f7e22405da0
inokappa <inokara@gmail.com>
2021-01-17 16:30:07 +0900

    debug (21)

--- src/scripts/common.sh
+++ src/scripts/common.sh
@@ -11,14 +11,12 @@ Common() {
     exit 1
   fi
 
-  if [ -z "${PARAM_GROUPID}" ] && [ -z "${PARAM_GROUPID_ENV}" ]; then
+  if [ -z "${PARAM_GROUPID}" ]; then
     GROUPID=$(${AWS_COMMAND} ec2 describe-security-groups \
       --query 'SecurityGroups[].[Tags[?Key==`${PARAM_TAG_KEY}`] | [0].Value, GroupId]' \
       --output text | grep ${PARAM_TAG_VALUE} | awk '{print $2}')
     [[ -n "${GROUPID}" ]] || (echo "Could not determine Security Group ID" && exit 0);
     PARAM_GROUPID=${GROUPID}
-  elif [ -n "${PARAM_GROUPID_ENV}" ] && [ -z "${PARAM_GROUPID}" ];then
-    PARAM_GROUPID=${PARAM_GROUPID_ENV}
   fi
 }
 
