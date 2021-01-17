e787012fc096c03fd989c1b6f29e4bc5d2529e79
inokappa <inokara@gmail.com>
2021-01-17 13:34:40 +0900

    change aws command

--- src/scripts/add.sh
+++ src/scripts/add.sh
@@ -5,7 +5,7 @@ Add() {
 
   echo "Allowing CircleCI to access port ${PARAM_PORT} from IP ${IP} to the security group ${PARAM_GROUPID}"
 
-  aws ec2 authorize-security-group-ingress --group-id "${PARAM_GROUPID}" --ip-permissions \
+  ${AWS_COMMAND} ec2 authorize-security-group-ingress --group-id "${PARAM_GROUPID}" --ip-permissions \
     $(echo '[{"IpProtocol": "tcp", "FromPort": 443, "ToPort": 443, "IpRanges": [{"CidrIp": "", "Description": "CircleCI"}]}]' \
       | jq -c '.[].IpRanges[].CidrIp="'${IP}/${PARAM_MASK}'"|.[].IpRanges[].Description="'${PARAM_DESC}'"|.[].FromPort='${PARAM_PORT}'|.[].ToPort='${PARAM_PORT}'')
 }
